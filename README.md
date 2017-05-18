355cc9c29cda7186b37410b7e543e49292116929
Will Dixon <dixonwille@gmail.com>
2017-05-17 21:13:06 -0400

    Get the skeleton for error handling down

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # VARS
-Vulnerability Assessment Reference System
+
+**V**ulnerability **A**ssessment **R**eference **S**ystem
+
+## Installing Dependencies
+
+We try to keep a stable development environment. Please use `govendor` to make sure you have the same version of our external packages.
+
+```bash
+#Install the govendor tool
+go get github.com/kardianos/govendor
+#Download the dependencies into the vendoring folder
+govendor sync
+```
---
+++ errors.go
@@ -0,0 +1,80 @@
+package vars
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
+
+type errType int
+
+const (
+	noRowsInserted errType = iota
+	noRowsUpdated
+)
+
+var (
+	//ErrNoRowsInserted is used when there were not any rows inserted into the table
+	ErrNoRowsInserted = errors.New("No rows were inserted")
+	//ErrNoRowsUpdated is used when there were not any rows updated in the table
+	ErrNoRowsUpdated = errors.New("No rows were updated")
+	//ErrGenericVars is used when the error is too generic
+	ErrGenericVars = errors.New("Something went wrong")
+)
+
+//Err is an error that occured inside the vars package
+type Err struct {
+	parents []string
+	err     error
+}
+
+//Creates a new VARS error based on the type
+func newErr(errT errType, parents ...string) Err {
+	err := new(Err)
+	err.parents = parents
+	switch errT {
+	case noRowsInserted:
+		err.err = ErrNoRowsInserted
+	case noRowsUpdated:
+		err.err = ErrNoRowsUpdated
+	default:
+		err.err = ErrGenericVars
+	}
+	return *err
+}
+
+//Creates a new VARS error using the error given
+//If the error given was already a VARS error Prepend the parents and don't change the error
+func newErrFromErr(err error, parents ...string) Err {
+	if varsErr, ok := err.(Err); ok {
+		return Err{
+			parents: append(parents, varsErr.parents...),
+			err:     varsErr.err,
+		}
+	}
+	return Err{
+		parents: parents,
+		err:     err,
+	}
+}
+
+//Error impliments the error interface
+func (e Err) Error() string {
+	return fmt.Sprintf("VARS: %s: %s", strings.Join(e.parents, ": "), e.err.Error())
+}
+
+//IsNoRowsError returns true if the error is caused by no rows being effected
+func (e Err) IsNoRowsError() bool {
+	if e.err.Error() == ErrNoRowsInserted.Error() || e.err.Error() == ErrNoRowsUpdated.Error() {
+		return true
+	}
+	return false
+}
+
+//IsNoRowsError returns true if the error is caused by no rows being effected
+func IsNoRowsError(err error) bool {
+	if varsErr, ok := err.(Err); ok {
+		return varsErr.IsNoRowsError()
+	}
+	return false
+}
