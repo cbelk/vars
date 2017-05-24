52f975f13caebb988be9d1480a41ed56652fceaa
Christian Belk <christian.belk@acstechnologies.com>
2017-05-24 13:29:59 -0400

    fixed issue of getting vulnid after inserting new vuln. All errors are now packed into an Err/Errs. Added function to enforce unique vuln names. Added function to retrieve a vulnid given a vuln name

--- vars.go
+++ vars.go
@@ -11,8 +11,10 @@ type sqlStatement int
 
 const (
 	ssActiveSystems sqlStatement = iota
+	ssCheckName
 	ssDecomSystem
 	ssGetVulnDates
+	ssGetVulnId
 	ssInsertAffected
 	ssInsertEmployee
 	ssInsertExploit
@@ -38,15 +40,17 @@ var (
 	queries      map[sqlStatement]*sql.Stmt
 	queryStrings = map[sqlStatement]string{
 		ssActiveSystems:   "SELECT sysid, sysname, systype, opsys, location, description FROM systems WHERE state='active';",
+		ssCheckName:       "SELECT vulnid FROM vuln WHERE vulnname=$1;",
 		ssDecomSystem:     "UPDATE systems SET state='decommissioned' WHERE sysname=$1;",
 		ssGetVulnDates:    "SELECT published, initiated, mitigated FROM dates WHERE vulnid=$1;",
+		ssGetVulnId:       "SELECT vulnid FROM vuln WHERE vulnname=$1;",
 		ssInsertAffected:  "INSERT INTO affected (vulnid, sysid) VALUES ($1, $2);",
 		ssInsertEmployee:  "INSERT INTO emp (firstname, lastname, email) VALUES ($1, $2, $3);",
 		ssInsertExploit:   "INSERT INTO exploits (vulnid, exploitable, exploit) VALUES ($1, $2, $3);",
 		ssInsertRefers:    "INSERT INTO ref (vulnid, url) VALUES ($1, $2);",
 		ssInsertSystem:    "INSERT INTO systems (sysname, systype, opsys, location, description, state) VALUES ($1, $2, $3, $4, $5, $6);",
 		ssInsertTicket:    "INSERT INTO tickets (vulnid, ticket) VALUES ($1, $2);",
-		ssInsertVuln:      "INSERT INTO vuln (vulnname, cve, finder, initiator, summary, test, mitigation) VALUES ($1, $2, $3, $4, $5, $6, $7);",
+		ssInsertVuln:      "INSERT INTO vuln (vulnname, cve, finder, initiator, summary, test, mitigation) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING vulnid;",
 		ssUpdateAffected:  "UPDATE affected SET sysid=$1 WHERE vulnid=$2 AND sysid=$3;",
 		ssUpdateCve:       "UPDATE vuln SET cve=$1 WHERE vulnid=$2;",
 		ssUpdateCvss:      "UPDATE impact SET cvss=$1 WHERE vulnid=$2;",
@@ -64,6 +68,7 @@ var (
 		ssInsertExploit:   "InsertExploit",
 		ssInsertRefers:    "InsertRef",
 		ssInsertTicket:    "InsertTicket",
+		ssInsertVuln:      "InsertVulnerability",
 		ssUpdateAffected:  "UpdateAffected",
 		ssUpdateCve:       "UpdateCve",
 		ssUpdateCvss:      "UpdateCvss",
@@ -80,7 +85,7 @@ var (
 
 // Employee holds information about an employee
 type Employee struct {
-	ID        int
+	ID        int64
 	FirstName string
 	LastName  string
 	Email     string
@@ -88,7 +93,7 @@ type Employee struct {
 
 // System holds information about systems in the environment.
 type System struct {
-	ID          int
+	ID          int64
 	Name        string
 	Type        string // Server, router, switch, etc
 	OpSys       string
@@ -106,7 +111,7 @@ type VulnDates struct {
 
 // Vulnerability holds information about a discovered vulnerability and the vulnerability assessment.
 type Vulnerability struct {
-	ID          int
+	ID          int64
 	Name        string
 	Cve         sql.NullString
 	Cvss        float32        // CVSS score
@@ -130,7 +135,10 @@ func AddSystem(db *sql.DB, sys *System) error {
 	if rows, _ := res.RowsAffected(); rows < 1 {
 		return newErr(noRowsInserted, "AddSystem")
 	}
-	return err
+	if err != nil {
+		return newErrFromErr(err, "AddSystem")
+	}
+	return nil
 }
 
 // AddEmployee inserts a new employee into the VARS database.
@@ -139,15 +147,36 @@ func AddEmployee(db *sql.DB, emp *Employee) error {
 	if rows, _ := res.RowsAffected(); rows < 1 {
 		return newErr(noRowsInserted, "AddEmployee")
 	}
-	return err
+	if err != nil {
+		return newErrFromErr(err, "AddEmployee")
+	}
+	return nil
 }
 
 // AddVulnerability starts a new vulnerability assessment by inserting a new vulnerability.
 func AddVulnerability(db *sql.DB, vuln *Vulnerability) error {
 	var errs Errs
+
+	// Check if vulnerability name is available
+	a, er := NameIsAvailable(vuln.Name)
+	if er != nil {
+		return newErrFromErr(er, "AddVulnerability")
+	}
+	if !a {
+		return newErr(nameNotAvailable, "AddVulnerability")
+	}
+
+	// Setting values in the vuln table
+	var id int64
+	err := queries[ssInsertVuln].QueryRow(vuln.Name, vuln.Cve, vuln.Finder, vuln.Initiator, vuln.Summary, vuln.Test, vuln.Mitigation).Scan(&id)
+	if err != nil {
+		return newErrFromErr(err, "AddVulnerability", "ssInsertVuln")
+	}
+	vuln.ID = id
+
 	tx, err := db.Begin()
 	if err != nil {
-		return err
+		return newErrFromErr(err, "AddVulnerability")
 	}
 	rollback := true
 	defer func() {
@@ -155,15 +184,9 @@ func AddVulnerability(db *sql.DB, vuln *Vulnerability) error {
 			tx.Rollback()
 		}
 	}()
-	res, err := tx.Stmt(queries[ssInsertVuln]).Exec(vuln.Name, vuln.Cve, vuln.Finder, vuln.Initiator, vuln.Summary, vuln.Test, vuln.Mitigation)
-	if err != nil {
-		return err
-	}
-	if rows, _ := res.RowsAffected(); rows < 1 {
-		errs.append(noRowsInserted, "AddVulnerability")
-	}
 
 	var e interface{}
+
 	// Setting values in the impact table
 	e = SetImpact(tx, vuln)
 	if ve, ok := e.(Err); ok {
@@ -221,57 +244,86 @@ func DecommissionSystem(db *sql.DB, name string) error {
 	if rows, _ := res.RowsAffected(); rows < 1 {
 		return newErr(noRowsUpdated, "DecommissionSystem")
 	}
-	return err
+	if err != nil {
+		return newErrFromErr(err, "DecommissionSystem")
+	}
+	return nil
 }
 
 // GetActiveSystems returns a pointer to a slice of System types representing the systems that are currently active.
-func GetActiveSystems(db *sql.DB) (*[]System, error) {
+func GetActiveSystems() (*[]System, error) {
 	systems := []System{}
 	rows, err := queries[ssActiveSystems].Query()
 	if err != nil {
-		return &systems, err
+		return &systems, newErrFromErr(err, "GetActiveSystems")
 	}
 	defer rows.Close()
 	for rows.Next() {
 		var sys System
 		if err := rows.Scan(&sys.ID, &sys.Name, &sys.Type, &sys.OpSys, &sys.Location, &sys.Description); err != nil {
-			return &systems, err
+			return &systems, newErrFromErr(err, "GetActiveSystems", "rows.Scan")
 		}
 		systems = append(systems, sys)
 	}
 	if err := rows.Err(); err != nil {
-		return &systems, err
+		return &systems, newErrFromErr(err, "GetActiveSystems")
 	}
 	return &systems, nil
 }
 
+func GetVulnId(vname string) (int64, error) {
+	var id int64
+	err := queries[ssGetVulnId].QueryRow(vname).Scan(&id)
+	if err != nil {
+		return id, newErrFromErr(err, "GetVulnId")
+	}
+	return id, nil
+}
+
 // InsertAffected will insert a new row into the affected table with key (vid, sid).
-func InsertAffected(tx *sql.Tx, vid int, sid int) Err {
+func InsertAffected(tx *sql.Tx, vid int64, sid int) Err {
 	return execUpdates(tx, ssInsertAffected, vid, sid)
 }
 
 // InsertRef will insert a new row into the ref table with key (vid, url).
-func InsertRef(tx *sql.Tx, vid int, url string) Err {
+func InsertRef(tx *sql.Tx, vid int64, url string) Err {
 	return execUpdates(tx, ssInsertRefers, vid, url)
 }
 
 // InsertTicket will insert a new row into the ticket table with key (vid, ticket).
-func InsertTicket(tx *sql.Tx, vid int, ticket string) Err {
+func InsertTicket(tx *sql.Tx, vid int64, ticket string) Err {
 	return execUpdates(tx, ssInsertTicket, vid, ticket)
+}
+
+func InsertVulnerability(tx *sql.Tx, vname, cve string, finder, initiator int, summary, test, mitigation string) error {
+	return execUpdates(tx, ssInsertVuln, vname, cve, finder, initiator, summary, test, mitigation)
 }
 
 // IsVulnOpen returns true if the Vulnerability associated with the passed ID is still open,
 // false otherwise.
-func IsVulnOpen(db *sql.DB, vid int) (bool, error) {
+func IsVulnOpen(vid int64) (bool, error) {
 	var vd VulnDates
 	err := queries[ssGetVulnDates].QueryRow(vid).Scan(&vd.Published, &vd.Initiated, &vd.Mitigated)
 	if err != nil {
-		return false, err
+		return false, newErrFromErr(err, "IsVulnOpen")
 	}
 	if vd.Mitigated.Valid {
 		return false, nil
 	}
 	return true, nil
+}
+
+// NameIsAvailable returns true if the vulnerability name is available, false otherwise.
+func NameIsAvailable(vname string) (bool, error) {
+	var vid int64
+	err := queries[ssCheckName].QueryRow(vname).Scan(&vid)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return true, nil
+		}
+		return false, newErrFromErr(err, "NameIsAvailable")
+	}
+	return false, nil
 }
 
 // SetImpact updates the CVSS score and links and the Corporate Risk Score for a vulnerability.
@@ -385,71 +437,71 @@ func SetTickets(tx *sql.Tx, vuln *Vulnerability) error {
 }
 
 // UpdateAffected will update the system ID associated with the (vid, oldSid) row to newSid.
-func UpdateAffected(tx *sql.Tx, vid, oldSid, newSid int) Err {
+func UpdateAffected(tx *sql.Tx, vid int64, oldSid, newSid int) Err {
 	return execUpdates(tx, ssUpdateAffected, newSid, vid, oldSid)
 }
 
 // UpdateCve will update the CVE for the given vulnerability ID.
-func UpdateCve(tx *sql.Tx, vid int, cve string) Err {
+func UpdateCve(tx *sql.Tx, vid int64, cve string) Err {
 	return execUpdates(tx, ssUpdateCve, cve, vid)
 }
 
 // UpdateCvss will update the CVSS score for the given vulnerability ID.
-func UpdateCvss(tx *sql.Tx, vid int, cvss float32) Err {
+func UpdateCvss(tx *sql.Tx, vid int64, cvss float32) Err {
 	return execUpdates(tx, ssUpdateCvss, cvss, vid)
 }
 
 // UpdateCvssLink will update the link to the CVSS score for the given vulnerability ID.
-func UpdateCvssLink(tx *sql.Tx, vid int, cvssLink string) Err {
+func UpdateCvssLink(tx *sql.Tx, vid int64, cvssLink string) Err {
 	return execUpdates(tx, ssUpdateCvssLink, cvssLink, vid)
 }
 
 // UpdateCorpScore will update the corporate score for the given vulnerability ID.
-func UpdateCorpScore(tx *sql.Tx, vid int, cscore float32) Err {
+func UpdateCorpScore(tx *sql.Tx, vid int64, cscore float32) Err {
 	return execUpdates(tx, ssUpdateCorpScore, cscore, vid)
 }
 
 // UpdateExploit will update the exploit and the exploitable column for the given vulnerability ID.
 // To set the exploitable column to false and have a NULL value for the exploita column, pass in
 // an empty string to exploit.
-func UpdateExploit(tx *sql.Tx, vid int, exploit string) Err {
+func UpdateExploit(tx *sql.Tx, vid int64, exploit string) Err {
 	s := toNullString(exploit)
 	return execUpdates(tx, ssUpdateExploit, s.Valid, s, vid)
 }
 
 // UpdateInitDate will update the date that the vulnerability assessment was initiated for the given vulnerability ID.
-func UpdateInitDate(tx *sql.Tx, vid int, initDate string) Err {
+func UpdateInitDate(tx *sql.Tx, vid int64, initDate string) Err {
 	return execUpdates(tx, ssUpdateInitDate, initDate, vid)
 }
 
 // UpdateMitDate will update the date that the vulnerability assessment was mitigated for the given vulnerability ID.
 // To set the mitigation date to NULL, pass in an empty string for mitDate.
-func UpdateMitDate(tx *sql.Tx, vid int, mitDate string) Err {
+func UpdateMitDate(tx *sql.Tx, vid int64, mitDate string) Err {
 	s := toNullString(mitDate)
 	return execUpdates(tx, ssUpdateMitDate, s, vid)
 }
 
 // UpdatePubDate will update the date that the vulnerability was published for the given vulnerability ID.
 // To set the published date to NULL, pass in an empty string for pubDate.
-func UpdatePubDate(tx *sql.Tx, vid int, pubDate string) Err {
+func UpdatePubDate(tx *sql.Tx, vid int64, pubDate string) Err {
 	s := toNullString(pubDate)
 	return execUpdates(tx, ssUpdatePubDate, s, vid)
 }
 
 // UpdateRefers will update the url associated with the (vid, oldUrl) row to newUrl.
-func UpdateRefers(tx *sql.Tx, vid int, oldUrl, newUrl string) Err {
+func UpdateRefers(tx *sql.Tx, vid int64, oldUrl, newUrl string) Err {
 	return execUpdates(tx, ssUpdateRefers, newUrl, vid, oldUrl)
 }
 
 // UpdateTicket will update the ticket associated with the (vid, oldTicket) row to newTicket.
-func UpdateTicket(tx *sql.Tx, vid int, oldTicket, newTicket string) Err {
+func UpdateTicket(tx *sql.Tx, vid int64, oldTicket, newTicket string) Err {
 	return execUpdates(tx, ssUpdateTicket, newTicket, vid, oldTicket)
 }
 
 // execUpdates executes the query referenced by ss in the queries map and returns any errors.
 func execUpdates(tx *sql.Tx, ss sqlStatement, args ...interface{}) Err {
 	var err Err
-	res, e := tx.Stmt(queries[ss]).Exec(args)
+	res, e := tx.Stmt(queries[ss]).Exec(args...)
 	if e != nil {
 		return newErrFromErr(e, execNames[ss])
 	}
