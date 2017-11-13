ee93ab7af16cdaf7269a289f45b9d7a5e6225d8b
Christian Belk <christian.belk@acstechnologies.com>
2017-11-13 13:30:29 -0500

    modified install script to handle passwords better

--- setupDB.sh
+++ setupDB.sh
@@ -13,16 +13,32 @@ echo '[+] Creating system user for VARS ...'
 addgroup --system $dbuser
 adduser --system --ingroup $dbuser $dbuser
 
+# Get password for new postgres user
+matched=false
+while ! $matched; do
+    read -e -s -p "Enter the password for the VARS database user: " p1
+    echo ""
+    read -e -s -p "Enter it again: " p2
+    echo ""
+    if [ "$p1" == "$p2" ]; then
+        pswd="$p1"
+        matched=true
+    else
+        echo "[-] Passwords did not match"
+    fi
+done
+
 # Create postgresql user
 echo ' [+] Creating postgresql user "vars" ...'
-su postgres -c "createuser --createdb --pwprompt vars"
+su postgres -c "createuser --createdb vars"
+tq="ALTER USER vars WITH PASSWORD '"$pswd"';"
+su postgres -c "psql -c \"$tq\""
 
 # Create the .pgpass file
-read -e -s -p "Enter the password again so it can be added to .pgpass: " pswd
 tmp="/home/$dbuser/.pgpass"
 touch $tmp
 chmod 0600 $tmp
-echo "localhost:5432:vams:vams:$pswd" > $tmp
+echo "localhost:5432:$dbname:$dbuser:$pswd" > $tmp
 
 ## Database Setup ##
 
@@ -35,6 +51,7 @@ sudo -u "$dbuser" psql < $(pwd)/vars.db >> db-creation.out 2>&1
 
 # Setup vars config
 mkdir /etc/vars
-cp `pwd`/vars.conf /etc/vars
+#cp `pwd`/vars.conf /etc/vars
+echo -e "{\n\t\"user\" : \"$dbuser\",\n\t\"pass\" : \"$pswd\",\n\t\"name\" : \"$dbname\",\n\t\"host\" : \"localhost\",\n\t\"port\" : \"5432\"\n}" > /etc/vars/vars.conf
 chgrp "$dbuser" /etc/vars/vars.conf
-chmod 644 /etc/vars/vars.conf
+chmod 640 /etc/vars/vars.conf
