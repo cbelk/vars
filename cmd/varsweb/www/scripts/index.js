884b55a81c4dc52e360c62402db32466fd67590d
Christian Belk <christian.belk@acstechnologies.com>
2017-12-21 20:25:35 -0500

    created the index page

---
+++ cmd/varsweb/www/scripts/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('#vars-index-vuln-card').on('click', function() {
+        window.location.href = '/vulnerability';
+    });
+    $('#vars-index-emp-card').on('click', function() {
+        window.location.href = '/employee';
+    });
+});
